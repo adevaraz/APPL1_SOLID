b5efa8c80022dc4904b7582747870a5ff71278ca
adevaraz <zaraveda.zv@gmail.com>
2020-06-28 22:02:04 +0700

    create main

--- src/_02_GraphicEditor/Program.java
+++ src/_02_GraphicEditor/Program.java
@@ -1,7 +1,18 @@
 package _02_GraphicEditor;
 
+import _02_GraphicEditor.Interface.Shape;
+import _02_GraphicEditor.Model.*;
+
 public class Program {
-	public static void main() {
+	public static void main(String[] args) {
 		
+		GraphicEditor graphicEditor = new GraphicEditor();
+		Shape circle = new Circle();
+		Shape rect = new Rectangle();
+		Shape square = new Square();
+		
+		graphicEditor.drawShape(circle);
+		graphicEditor.drawShape(rect);
+		graphicEditor.drawShape(square);
 	}
 }
