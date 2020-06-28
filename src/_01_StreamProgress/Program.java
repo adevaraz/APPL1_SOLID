6ceb6314d2018662a903397e2e1104c8a063907b
adevaraz <zaraveda.zv@gmail.com>
2020-06-28 21:38:22 +0700

    create main

--- src/_01_StreamProgress/Program.java
+++ src/_01_StreamProgress/Program.java
@@ -1,7 +1,16 @@
 package _01_StreamProgress;
 
+import _01_StreamProgress.Model.*;
+
 public class Program {
-	public static void main() {
+	public static void main(String[] args) {
+		File file = new File("File", 10, 5);
+		Music music = new Music("EXO", "Universe", 10, 6);
 		
+		StreamProgressInfo streamFileInfo = new StreamProgressInfo(file);
+		StreamProgressInfo streamMusicInfo = new StreamProgressInfo(music);
+		
+		System.out.println(streamFileInfo.calculateCurrentPercent());
+		System.out.println(streamFileInfo.calculateCurrentPercent());
 	}
 }
